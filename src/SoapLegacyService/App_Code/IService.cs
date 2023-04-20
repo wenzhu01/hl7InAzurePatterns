662b3e0147b42346f68c2b32ae0ec6f58ff54688
Hugo Girard <hugirard@microsoft.com>
2023-04-20 14:59:39 -0400

    add WCF

---
+++ src/SoapLegacyService/App_Code/IService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+[ServiceContract]
+public interface IHl7Service
+{
+	[OperationContract]
+	string ReceiveHL7Message(HL7Message hl7Message);	
+}
+
+[DataContract]
+public class HL7Message
+{
+	[DataMember]
+    public string Message { get; set; }
+}
---
+++ src/SoapLegacyService/App_Code/Service.cs
@@ -0,0 +1,11 @@
+﻿public class Service : IHl7Service
+{
+    public string ReceiveHL7Message(HL7Message hl7Message)
+    {
+        // The logic here is not needed, this will be used as a passthru in APIM to modernize
+        // So for now we will only return the message but technically here will be the validation etc 
+        // We are moving this in the cloud
+        string message = hl7Message.Message;
+        return message;
+    }
+}
