c9b026bbfb6863d48cf337bef6ebce045ec3ff1f
Hugo Girard <hugirard@microsoft.com>
2023-04-21 15:22:52 -0400

    added mllp wrapper

--- src/MllP/client/Program.cs
+++ src/MllP/client/Program.cs
@@ -3,9 +3,28 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
+char END_OF_BLOCK = '\u001c';
+char START_OF_BLOCK = '\u000b';
+char CARRIAGE_RETURN = (char)13;
+
+
 TcpClient tcpClient = new TcpClient();
 NetworkStream networkStream = null ;
 
+var testHl7MessageToTransmit = new StringBuilder();
+testHl7MessageToTransmit.Append(START_OF_BLOCK)
+        .Append("MSH|^~\\&|AcmeHIS|StJohn|CATH|StJohn|20061019172719||ORM^O01|MSGID12349876|P|2.3")
+        .Append(CARRIAGE_RETURN)
+        .Append("PID|||20301||Durden^Tyler^^^Mr.||19700312|M|||88 Punchward Dr.^^Los Angeles^CA^11221^USA|||||||")
+        .Append(CARRIAGE_RETURN)
+        .Append("PV1||O|OP^^||||4652^Paulson^Robert|||OP|||||||||9|||||||||||||||||||||||||20061019172717|20061019172718")
+        .Append(CARRIAGE_RETURN)
+        .Append("ORC|NW|20061019172719")
+        .Append(CARRIAGE_RETURN)
+        .Append("OBR|1|20061019172719||76770^Ultrasound: retroperitoneal^C4|||12349876")
+        .Append(CARRIAGE_RETURN)
+        .Append(END_OF_BLOCK)
+        .Append(CARRIAGE_RETURN);
 try
 {
     //initiate a TCP client connection to local loopback address at port 1080
@@ -17,28 +36,46 @@ try
     networkStream = tcpClient.GetStream();
 
     //use UTF-8 and either 8-bit encoding due to MLLP-related recommendations
-    var messageToTransmit = "Hello from Client";
-    var byteBuffer = Encoding.UTF8.GetBytes(messageToTransmit);
+    //var messageToTransmit = Encoding.UTF8.GetBytes(testHl7MessageToTransmit.ToString());
+    var byteBuffer = Encoding.UTF8.GetBytes(testHl7MessageToTransmit.ToString());
 
     //send a message through this connection using the IO stream
     networkStream.Write(byteBuffer, 0, byteBuffer.Length);    
 
-    var bytesReceivedFromServer = await networkStream.ReadAsync(byteBuffer, 0, byteBuffer.Length);
-    // Our server for this example has been designed to echo back the message
-    // keep reading from this stream until the message is echoed back
-    while (bytesReceivedFromServer < byteBuffer.Length)
-    {
-        bytesReceivedFromServer = await networkStream.ReadAsync(byteBuffer, 0, byteBuffer.Length);
-        if (bytesReceivedFromServer == 0)
-        {
-            //exit the reading loop since there is no more data
-            break;
-        }
-    }  
+    Console.WriteLine("Data was sent data to server successfully....");
 
-    var receivedMessage = Encoding.UTF8.GetString(byteBuffer);
+    var receiveMessageByteBuffer = Encoding.UTF8.GetBytes(testHl7MessageToTransmit.ToString());
+    var bytesReceivedFromServer = await networkStream.ReadAsync(receiveMessageByteBuffer, 0, receiveMessageByteBuffer.Length);
+
+    while (bytesReceivedFromServer > 0)
+    {
+        if (networkStream.CanRead)
+        {
+            bytesReceivedFromServer = await networkStream.ReadAsync(receiveMessageByteBuffer, 0, receiveMessageByteBuffer.Length);
+            if (bytesReceivedFromServer == 0)
+            {
+                break;
+            }
+        }
+        
+    }
+    var receivedMessage = Encoding.UTF8.GetString(receiveMessageByteBuffer);
 
     Console.WriteLine("Received message from server: {0}", receivedMessage);
+    // var bytesReceivedFromServer = await networkStream.ReadAsync(byteBuffer, 0, byteBuffer.Length);
+    // // Our server for this example has been designed to echo back the message
+    // // keep reading from this stream until the message is echoed back
+    // while (bytesReceivedFromServer < byteBuffer.Length)
+    // {
+    //     bytesReceivedFromServer = await networkStream.ReadAsync(byteBuffer, 0, byteBuffer.Length);
+    //     if (bytesReceivedFromServer == 0)
+    //     {
+    //         //exit the reading loop since there is no more data
+    //         break;
+    //     }
+    // }  
+
+    // var receivedMessage = Encoding.UTF8.GetString(byteBuffer);
 
     Console.WriteLine("Press any key to exit program...");
     Console.ReadLine();      
--- src/MllP/mllpServer/Service/MllpServer.cs
+++ src/MllP/mllpServer/Service/MllpServer.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 namespace mllpServer;
 
@@ -9,7 +10,7 @@ public interface IMllpServer
     void Initialize();
 
     void Stop();
-    
+
     Task WaitingForClientAsync();
 }
 
@@ -22,6 +23,15 @@ public class MllpServer : IMllpServer
 
     private TcpClient acceptedClient = null;
     private NetworkStream networkStream = null;
+
+    private readonly char END_OF_BLOCK = '\u001c';
+    private readonly char START_OF_BLOCK = '\u000b';
+    private readonly char CARRIAGE_RETURN = (char)13;
+
+    private readonly int MESSAGE_CONTROL_ID_LOCATION = 9;
+
+    private readonly char FIELD_DELIMITER = '|';
+
 
     public MllpServer(IConfiguration configuration, ILogger<MllpServer> logger)
     {
@@ -38,7 +48,7 @@ public class MllpServer : IMllpServer
     {
         try
         {
-            _tcpListener = new TcpListener(IPAddress.Any, 1080);
+            _tcpListener = new TcpListener(IPAddress.Any, PORT);
 
             _tcpListener.Start();
 
@@ -68,16 +78,48 @@ public class MllpServer : IMllpServer
             // Keep receiving data from client until connection is closed
             var totalBytesReceivedFromClient = 0;
             int bytesReceived;
+            string hl7Data = string.Empty;
 
             while ((bytesReceived = await networkStream.ReadAsync(_receivedByteBuffer, 0, _receivedByteBuffer.Length)) > 0)
             {
-                if (networkStream.CanWrite)
+
+                hl7Data += Encoding.UTF8.GetString(_receivedByteBuffer, 0, bytesReceived);
+
+                // Find start of MLLP frame, a VT character ...
+                var startOfMllpEnvelope = hl7Data.IndexOf(START_OF_BLOCK);
+                if (startOfMllpEnvelope >= 0)
                 {
-                    //echo the received data back to the client
-                    await networkStream.WriteAsync(_receivedByteBuffer, 0, bytesReceived);
+                    // Now look for the end of the frame, a FS character
+                    var end = hl7Data.IndexOf(END_OF_BLOCK);
+                    if (end >= startOfMllpEnvelope) //end of block received
+                    {
+                        //if both start and end of block are recognized in the data transmitted, then extract the entire message
+                        var hl7MessageData = hl7Data.Substring(startOfMllpEnvelope + 1, end - startOfMllpEnvelope);
+
+                        //create a HL7 acknowledgement message
+                        var ackMessage = GetSimpleAcknowledgementMessage(hl7MessageData);
+
+                        Console.WriteLine(ackMessage);
+
+                        //echo the received data back to the client
+                        var buffer = Encoding.UTF8.GetBytes(ackMessage);
+
+                        if (networkStream.CanWrite)
+                        {
+                            await networkStream.WriteAsync(buffer, 0, buffer.Length);
+
+                            Console.WriteLine("Ack message was sent back to the client...");
+                        }
+                    }
                 }
 
-                totalBytesReceivedFromClient += bytesReceived;
+                // if (networkStream.CanWrite)
+                // {
+                //     //echo the received data back to the client
+                //     await networkStream.WriteAsync(_receivedByteBuffer, 0, bytesReceived);
+                // }
+
+                // totalBytesReceivedFromClient += bytesReceived;
             }
 
             _logger.LogInformation("Echoed {0} bytes back to the client.", totalBytesReceivedFromClient);
@@ -109,4 +151,49 @@ public class MllpServer : IMllpServer
 
         }
     }
-}
+
+    private string GetSimpleAcknowledgementMessage(string incomingHl7Message) 
+    {
+        if (string.IsNullOrEmpty(incomingHl7Message))
+            throw new ApplicationException("Invalid HL7 message for parsing operation. Please check your inputs");
+
+        //retrieve the message control ID of the incoming HL7 message
+        var messageControlId = GetMessageControlID(incomingHl7Message);
+
+        //build an acknowledgement message and include the control ID with it
+        var ackMessage = new StringBuilder();
+        ackMessage = ackMessage.Append(START_OF_BLOCK)
+            .Append("MSH|^~\\&|||||||ACK||P|2.2")
+            .Append(CARRIAGE_RETURN)
+            .Append("MSA|AA|")
+            .Append(messageControlId)
+            .Append(CARRIAGE_RETURN)
+            .Append(END_OF_BLOCK)
+            .Append(CARRIAGE_RETURN);
+
+        return ackMessage.ToString();
+    }    
+
+    private string GetMessageControlID(string incomingHl7Message) 
+    {
+
+            var fieldCount = 0;
+            //parse the message into segments using the end of segment separter
+            var hl7MessageSegments = incomingHl7Message.Split(CARRIAGE_RETURN);
+
+            //tokenize the MSH segment into fields using the field separator
+            var hl7FieldsInMshSegment = hl7MessageSegments[0].Split(FIELD_DELIMITER);
+
+            //retrieve the message control ID in order to reply back with the message ack
+            foreach (var field in hl7FieldsInMshSegment)
+            {
+                if (fieldCount == MESSAGE_CONTROL_ID_LOCATION)
+                {
+                    return field;
+                }
+                fieldCount++;
+            }
+
+            return string.Empty; //you can also throw an exception here if you wish
+        }    
+    }
